956cc72b7041aae5881964e44959177b84ed45de
KevinMatthew133 <lusiana030381@gmail.com>
2024-07-04 18:04:40 +0700

    tabel karyawan

---
+++ template/dashboard/karyawan_view.php
@@ -0,0 +1,54 @@
+<!--**********************************
+            Content body start
+***********************************-->
+<div class="content-body">
+            <!-- row -->
+			<div class="container-fluid">
+				<div class="row">
+					<div class="col-xl-12">
+						<div class="col card">
+							<div class="card-header">
+								<h2>Tabel Karyawan</h2>
+							</div>
+                            <table class="table card-body">
+								<tr>
+									<th>No</th>
+									<th>Nama</th>
+									<th>Telepon</th>
+									<th>Jabatan</th>
+									<th>Alamat</th>
+								</tr>
+								<tr>
+									<td>1</td>
+									<td>Kevin</td>
+									<td>081234567890</td>
+									<td>Karyawan</td>
+									<td>Jl nakula 6a</td>
+								</tr>
+								<tr>
+									<td>2</td>
+									<td>Galih</td>
+									<td>081234567890</td>
+									<td>Karyawan</td>
+									<td>Jl nakula 6a</td>
+								</tr>
+								<tr>
+									<td>3</td>
+									<td>Irfan</td>
+									<td>081234567890</td>
+									<td>Karyawan</td>
+									<td>Jl nakula 6a</td>
+								</tr>
+							</table>
+							<div class="card-footer">
+	
+							</div>
+						</div>
+					</div>
+				</div>
+            </div>
+        </div>
+
+<!--**********************************
+            Content body end
+        ***********************************-->
---
+++ template/karyawan.php
@@ -0,0 +1,14 @@
+<?php
+
+include "templates/header.php";
+include "templates/topbar.php";
+include "templates/leftbar.php";
+// include "templates/rightbar.php";
+// include "dashboard/dashboard_view.php";
+
+include "dashboard/karyawan_view.php";
+
+include "templates/copyright.php";
+include "templates/footer.php";
+
+?>
